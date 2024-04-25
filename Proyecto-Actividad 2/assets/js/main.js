a45353f550ef2d2a05a5aee551ec6f20636bfe69
ZahirMR <166728689+ZahirMR@users.noreply.github.com>
2024-04-24 23:32:34 -0400

    Add files via upload

---
+++ Proyecto-Actividad 2/assets/js/main.js
@@ -0,0 +1,11 @@
+;(function(){
+
+		// configuración del menú
+			$('#menuToggle, .menu-close').on('click', function(){
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open');
+			});
+
+
+})(jQuery)
